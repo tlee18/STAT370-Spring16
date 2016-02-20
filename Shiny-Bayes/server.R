55c1617300799b01e1a4c7c3f4a9d54ae26e6de8
Nicholas Horton <nicholasjhorton@gmail.com>
2016-02-20 09:37:33 -0500

    More changes.

--- Shiny-Bayes/server.R
+++ Shiny-Bayes/server.R
@@ -1,38 +1,19 @@
 shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+ 
+    require(mosaic)
+    trellis.par.set(theme=theme.mosaic())
 
-  library(mosaic)
-  library(ggvis)
-  library(lubridate)
-  library(shiny)
-  
-  load("occ.Rda")
-  
-  all_values <- function(x) {
-    if (is.null(x)) return(NULL) 
-    row <- occ[occ$id == x$id, ]
-    paste0(names(row), ": ", format(row), collapse = "<br />")
+    # generate the desired plot
+    if (input$posterior == "No") {
+      print(plotDist('beta', params=list(input$alpha, input$beta), ylim=c(0, 4), 
+                     main="Prior distribution", lwd=2)) 
+    } else if (input$posterior == "Yes") {
+      print(plotDist('beta', 
+            params=list((input$alpha + input$y), (input$beta + input$n)), 
+            main="Posterior distribution", lwd=2))
+    } 
+    })
   }
+)
 
-  lb <- linked_brush(keys = 1:nrow(occ), "red")
-  # Change the colour of the points, then click on the stop sign
-  occ %>%
-    ggvis(~time, ~Lane.1.Occ, key := ~id) %>%
-    layer_points(fill := lb$fill, size.brush := 100) %>%
-    add_tooltip(all_values, "hover") %>%
-    lb$input() %>%
-    bind_shiny("ggvis1", "ggvis_ui1")
-  
-  occ %>%
-    ggvis(~Lane.1.Occ, ~Lane.1.Flow, key := ~id) %>%
-    layer_points(size.brush := 100) %>%
-    add_tooltip(all_values, "hover") %>%
-    layer_points(fill := "red", data = reactive(occ[lb$selected(), ])) %>%
-    bind_shiny("ggvis2", "ggvis_ui2")
-  
-  occ %>% 
-    ggvis(y = ~Lane.1.Flow, x = ~time, key := ~id) %>%
-    layer_points() %>%
-    add_tooltip(all_values, "hover") %>%
-    layer_points(fill := "red", data = reactive(occ[lb$selected(), ])) %>%
-    bind_shiny("ggvis3", "ggvis_ui3")
-})
--- Shiny-Bayes/ui.R
+++ Shiny-Bayes/ui.R
@@ -1,15 +1,23 @@
-library(ggvis)
-shinyUI(bootstrapPage(
-  p("Rice 3rd edition Exercise 10.50: Highway occupancy"),
-  p("Brush by selecting points in first plot to color, hover in any to display values"),
-  
-  uiOutput("ggvis_ui1"),
-  ggvisOutput("ggvis1"),
-  
-  uiOutput("ggvis_ui2"),
-  ggvisOutput("ggvis2"),
-  
-  uiOutput("ggvis_ui3"),
-  ggvisOutput("ggvis3")
+library(shiny)
 
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Bayesian inference"),
+  p("Example E of section 3.5 of Rice 3rd edition, page 94-95"),
+
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("alpha", "alpha parameter for Beta prior:", 2,
+                   min = 1, max = 100),
+      numericInput("beta", "beta parameter for Beta prior:", 2,
+                   min = 1, max = 100),
+      numericInput("y", "number of successes observed:", 1,
+                   min = 1, max = 100),
+      numericInput("n", "out of this many trials (must be larger than # successes):", 3,
+                   min = 1, max = 100),
+      selectInput("posterior", "Display posterior?",
+                  choices = c("Yes", "No"), selected="No")),
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("distPlot"))
+  )
 ))
