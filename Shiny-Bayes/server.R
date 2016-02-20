807a44312bb123dc798d0380178d5cbc4d7c424e
Nicholas Horton <nicholasjhorton@gmail.com>
2016-02-20 11:17:05 -0500

    Finished Rice example E section 3.5 beta binomial Bayesian

--- Shiny-Bayes/server.R
+++ Shiny-Bayes/server.R
@@ -1,19 +1,23 @@
 shinyServer(function(input, output) {
-  output$distPlot <- renderPlot({
+  output$distPrior <- renderPlot({
  
     require(mosaic)
     trellis.par.set(theme=theme.mosaic())
 
-    # generate the desired plot
-    if (input$posterior == "No") {
-      print(plotDist('beta', params=list(input$alpha, input$beta), ylim=c(0, 4), 
-                     main="Prior distribution", lwd=2)) 
-    } else if (input$posterior == "Yes") {
-      print(plotDist('beta', 
-            params=list((input$alpha + input$y), (input$beta + input$n)), 
-            main="Posterior distribution", lwd=2))
-    } 
+    print(plotDist('beta', params=list(input$alpha, input$beta), 
+                     main="Prior distribution", xlim=c(0, 1), lwd=2)) 
     })
+  
+  output$distPost <- renderPlot({
+    
+    require(mosaic)
+    trellis.par.set(theme=theme.mosaic())
+    print(plotDist('beta', 
+                     params=list((input$alpha + input$y), (input$beta + input$n - input$y)), 
+                     main="Posterior distribution", xlim=c(0, 1), lwd=2))
+
+  })
+  
   }
 )
 
--- Shiny-Bayes/ui.R
+++ Shiny-Bayes/ui.R
@@ -4,20 +4,23 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Bayesian inference"),
   p("Example E of section 3.5 of Rice 3rd edition, page 94-95"),
+  p("beta prior, likelihood is binomial, posterior is beta"),
+  p("See Figure 3.16 for results from a beta(1, 1) prior and 13 successes out of 20 attempts"),
 
   sidebarLayout(
     sidebarPanel(
-      numericInput("alpha", "alpha parameter for Beta prior:", 2,
+      numericInput("alpha", "alpha parameter for Beta prior:", 1,
                    min = 1, max = 100),
-      numericInput("beta", "beta parameter for Beta prior:", 2,
+      numericInput("beta", "beta parameter for Beta prior:", 1,
                    min = 1, max = 100),
-      numericInput("y", "number of successes observed:", 1,
+      numericInput("y", "number of successes observed:", 13,
                    min = 1, max = 100),
-      numericInput("n", "out of this many trials (must be larger than # successes):", 3,
+      numericInput("n", "out of this many trials (must be larger than # successes):", 20,
                    min = 1, max = 100),
       selectInput("posterior", "Display posterior?",
                   choices = c("Yes", "No"), selected="No")),
     # Show a plot of the generated distribution
-    mainPanel(plotOutput("distPlot"))
+    mainPanel(plotOutput("distPrior"), 
+              plotOutput("distPost"))
   )
 ))
